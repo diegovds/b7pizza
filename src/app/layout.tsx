2275e1c85340edba44b232ddb70769ee796b83ef
Diego Viana <dsantos@inf.ufsm.br>
2025-05-20 17:22:25 -0300

    creating authentication dialog

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Cart } from '@/components/cart/cart'
+import { LoginAreaDialog } from '@/components/login-area/login-area-dialog'
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
@@ -30,6 +31,7 @@ export default function RootLayout({
       >
         {children}
         <Cart />
+        <LoginAreaDialog />
       </body>
     </html>
   )
