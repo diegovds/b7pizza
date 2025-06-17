7f2c84b1b6fb495052830ecf0101773dfe11b60a
Diego Viana <dsantos@inf.ufsm.br>
2025-06-17 18:25:31 -0300

    modifying purchase status

---
+++ src/app/success/page.tsx
@@ -0,0 +1,30 @@
+import { Header } from '@/components/layout/header'
+import { stripe } from '@/lib/stripe'
+import { redirect } from 'next/navigation'
+
+type Props = {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+}
+
+export default async function Page({ searchParams }: Props) {
+  const sessionId = (await searchParams).session_id as string
+  if (!sessionId) return redirect('/')
+
+  const paymentSession = await stripe.checkout.sessions.retrieve(sessionId)
+  if (!paymentSession) return redirect('/')
+
+  const custumerEmail = paymentSession.customer_email
+
+  return (
+    <div>
+      <Header />
+      <main className="container mx-auto mb-10 text-center">
+        <h1 className="text-2xl">Parabéns pela compra!</h1>
+        <h3 className="text-xl">
+          Em breve enviaremos um e-mail para <strong>{custumerEmail}</strong>{' '}
+          com mais informações.
+        </h3>
+      </main>
+    </div>
+  )
+}
