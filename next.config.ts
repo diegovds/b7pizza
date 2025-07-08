5b6ced2c508544d08b3357ab0e6fb8cfe23f0bb3
Diego Viana <dsantos@inf.ufsm.br>
2025-07-08 16:29:18 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [new URL(`${process.env.NEXT_PUBLIC_BASE_URL}/**`)],
   },
-}
+  eslint: {
+    // Ignora erros do ESLint ao fazer build
+    ignoreDuringBuilds: true,
+  },
 
 export default nextConfig
